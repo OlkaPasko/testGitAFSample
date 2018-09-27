ea4503337639d91099852ec6461ed261fd49e7b3
Olha Pasko <opask@softserveinc.com>
2018-09-27 13:10:28 +0300

    Add project

---
+++ AFSample/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  AFSample
+//
+//  Created by Olha Pasko on 9/21/18.
+//  Copyright © 2018 Olha Pasko. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request("https://httpbin.org/get")
+            .responseJSON {response in
+                print(response.request)
+                print(response.response)
+                print(response.data)
+                print(response.result)
+                
+                if let JSON = response.result.value as? [String: Any] {
+                    print("JSON: \(JSON)")
+                    print("Headers = \(JSON["headers"] as! [String: Any])")
+                }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
